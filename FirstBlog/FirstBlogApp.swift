9fc47b6634490618d4a0c3d114581c7babe44992
Robert Romero <robert02frontend@gmail.com>
2023-01-21 14:00:19 -0400

    Initial Commit

---
+++ FirstBlog/FirstBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstBlogApp.swift
+//  FirstBlog
+//
+//  Created by Robert Romero Medina on 21/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
